f499d4e878048a9a3a716dda67871f61a0a0f086
JaSi1998 <jan.sippl@outlook.com>
2024-01-30 12:08:29 +0100

    Register Function ready

--- chat_app/chat/admin.py
+++ chat_app/chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Message, Chat
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('text', 'created_at', 'author', 'receiver')
+    search_fields = ('text',)
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat_app/chat/views.py
+++ chat_app/chat/views.py
@@ -1,3 +1,40 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from.models import Chat, Message
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+@login_required(login_url='/login/')
+def index(request):
+    if request.method == 'POST':
+        print(f"Recieved data {request.POST['textmessage']}")
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages' : chatMessages})
+
+def login_view(request):
+    redirect = request.GET.get('next')
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password = request.POST.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get('redirect'))
+        else:
+            return render(request, 'auth/login.html',{'wrongPassword': True, 'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
+
+def register_view(request):
+    if request.method == 'POST':
+        if request.POST.get('username') is not None and request.POST.get('password') is not None:
+            user = User.objects.create_user(username=request.POST.get('username'), email=request.POST.get('email'), password = request.POST.get('password'))
+            user.first_name = request.POST.get('first_name')
+            user.last_name = request.POST.get('last_name')
+            user.save()
+            return HttpResponseRedirect('/login/')
+        else:
+            return render(request, '/register.html', {'missingData': True})
+    else:
+        return render(request, 'register/register.html')
